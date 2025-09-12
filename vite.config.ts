a6045a825bdbc6454340e316e37a30657f9b1323
modders02 <elmerpobs@gmail.com>
2025-09-13 00:41:52 +0800

    Add vite.config.js and fix build output to dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,30 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+   
+  },
   build: {
+<<<<<<< HEAD
     outDir: "dist", // production output folder
+=======
+    outDir: "dist",
+>>>>>>> 971db2a (Fix Vite build output directory and add vite.config.js)
   },
   plugins: [
     react(),
-  ],
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+}));
