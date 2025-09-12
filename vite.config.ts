c5c5b455ef890549f4dc5c6e25a0b8eac4e572d8
modders02 <elmerpobs@gmail.com>
2025-09-13 00:25:43 +0800

    Set dist as build output and remove dev-only plugin

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,9 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: "dist", // production output folder
-  },
+  outDir: "build",
+},
+
   plugins: [
     react(),
   ],
