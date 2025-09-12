afcf78e4d3642b50b0d01e587294425748680843
Distro <77967967+modders02@users.noreply.github.com>
2025-09-13 00:34:30 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,8 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-  outDir: "dist",
-},
-
+    outDir: "dist", // production output folder
+  },
   plugins: [
     react(),
   ],
